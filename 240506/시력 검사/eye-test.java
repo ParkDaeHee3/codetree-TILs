d2aa6fc6471846f1f360cf1eb6595ade19b50703
ParkDaeHee3 <156628364+ParkDaeHee3@users.noreply.github.com>
2024-05-06 14:19:36 +0900

    Title: 시력 검사, Time: 130ms, Memory: 11MB, Status: Wrong Answer - CodeTree

---
+++ 240506/시력 검사/eye-test.java
@@ -0,0 +1,25 @@
+import java.util.Scanner; 
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in); 
+
+        double a,b; 
+        a = sc.nextDouble();
+        b = sc.nextDouble(); 
+
+        if(a>=1.0 && b>=1.0){
+            System.out.println("High");
+        }
+         if(a<1.0 && a>=0.5){
+            System.out.println("Middle");
+        }
+         if(b<1.0 && b>=0.5){
+            System.out.println("Middle");
+        }
+        if(a<=0.4&&b<=0.4){
+            System.out.println("low");
+        }
+
+    }
+}
