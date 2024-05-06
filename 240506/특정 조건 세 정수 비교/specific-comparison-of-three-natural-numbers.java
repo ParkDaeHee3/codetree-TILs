3c14904e0ed6bede960592a32922c76426036460
ParkDaeHee3 <156628364+ParkDaeHee3@users.noreply.github.com>
2024-05-06 15:18:43 +0900

    Title: 특정 조건 세 정수 비교, Time: 123ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240506/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.java
+++ 240506/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.java
@@ -13,12 +13,11 @@ public class Main {
         if(a<b && a<c){
             System.out.printf("1 "); 
         }
-        else{
-            System.out.printf("0 ");
+        if(a==b && a==c ){
+        System.out.printf("1 "); 
         }
-
-        if(a==b && a==c){
-        System.out.println(1); 
+        if(a==b){
+        System.out.println("1 "+"0"); 
         }
         else{
             System.out.println(0);
