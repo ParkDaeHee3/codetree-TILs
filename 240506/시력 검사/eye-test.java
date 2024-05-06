e38c1b11cd72dabcaccfc96ba0f025a8007f5fd9
ParkDaeHee3 <156628364+ParkDaeHee3@users.noreply.github.com>
2024-05-06 14:54:06 +0900

    Title: 시력 검사, Time: 132ms, Memory: 11MB, Status: Wrong Answer - CodeTree

--- 240506/시력 검사/eye-test.java
+++ 240506/시력 검사/eye-test.java
@@ -11,15 +11,13 @@ public class Main {
         if(a>=1.0 && b>=1.0){
             System.out.println("High");
         }
-         if(b<=0.5 && a<=0.5){
+         if(a<1.0 && a>=0.5 && b<1.0 && b>=0.5){
             System.out.println("middle");
         }
-         if(0.4>=a && 0.4>=b){
+         if(a<0.5&& b<0.5) {
             System.out.println("Low");
         }
-        else{
-            System.out.println("Low");
-        }
+        
 
 
     }
