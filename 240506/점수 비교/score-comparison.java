f26e3ebe9ddaa16d4520c15f41765845d190a40b
ParkDaeHee3 <156628364+ParkDaeHee3@users.noreply.github.com>
2024-05-06 13:58:49 +0900

    Title: 점수 비교, Time: 133ms, Memory: 10MB, Status: Passed - CodeTree

--- 240506/점수 비교/score-comparison.java
+++ 240506/점수 비교/score-comparison.java
@@ -4,13 +4,13 @@ public class Main {
         // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
 
-        int a,b,c,d;
-        a = sc.nextInt();
-        b = sc.nextInt();
-        c  = sc.nextInt();
-        d  = sc.nextInt(); 
+        int m1,m2,e1,e2;
+        m1 = sc.nextInt();
+        e1 = sc.nextInt();
+        m2  = sc.nextInt();
+        e2 = sc.nextInt(); 
         
-        if(a>=c && b>=d){
+        if(m1>m2 && e1>e2){
             System.out.println(1);
         }
         else{
