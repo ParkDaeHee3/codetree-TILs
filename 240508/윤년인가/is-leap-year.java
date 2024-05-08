62d535934c21bbe2650c27783c50c28ccc771828
ParkDaeHee3 <156628364+ParkDaeHee3@users.noreply.github.com>
2024-05-08 13:10:45 +0900

    Title: 윤년인가, Time: 152ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240508/윤년인가/is-leap-year.java
+++ 240508/윤년인가/is-leap-year.java
@@ -8,9 +8,10 @@ public class Main {
         int y; 
         y = sc.nextInt();
 
-        if(y%4==0 && y%100==0 && y%400==0 ){
-            System.out.println("true");
+        if(y%4==0 &&y%100==0 && y%400==0 ){
+         System.out.println("true");
         }
+       
         else{
             System.out.println("false");
         }
