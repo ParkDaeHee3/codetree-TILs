315cf3c0eb91b8f1c8367da09ea821e63ca04a95
ParkDaeHee3 <156628364+ParkDaeHee3@users.noreply.github.com>
2024-05-09 21:54:13 +0900

    Title: 코로나 메뉴얼, Time: 127ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240509/코로나 메뉴얼/covid-manual.java
+++ 240509/코로나 메뉴얼/covid-manual.java
@@ -16,18 +16,18 @@ public class Main {
         String c =sc.next(); 
         temp3 = sc.nextInt(); 
 
-        if( a.equals(input1) && (temp1>=37&&temp2>=37) ){
-            System.out.println("E"); 
+        if( a.equals(input1) && temp1<37 ){
+            System.out.println("N"); 
         }
-        else if(b.equals(input1) &&(temp2>=37&&temp3>=37)){
-                 System.out.println("E"); 
+        else if(b.equals(input1) &&temp2<37){
+                 System.out.println("N"); 
                  }
-        else if (c.equals(input1) && (temp1>=37&&temp3>=37)){
-                  System.out.println("E"); 
+        else if (c.equals(input1) && temp3<37){
+                  System.out.println("N"); 
                 }
     
         else{
-            System.out.println("N");
+            System.out.println("E");
         }
     }
     
