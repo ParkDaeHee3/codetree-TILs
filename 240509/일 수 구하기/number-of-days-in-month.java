dcf69ce94df9eb38a789222635ea8d67be3f9b09
ParkDaeHee3 <156628364+ParkDaeHee3@users.noreply.github.com>
2024-05-09 12:46:02 +0900

    Title: 일 수 구하기, Time: 110ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240509/일 수 구하기/number-of-days-in-month.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+
+        Scanner sc = new Scanner(System.in); 
+
+        int a; 
+        
+        a = sc.nextInt();
+
+        if(a==2)
+        
+           {
+            System.out.println("28");
+        }
+        else{
+            System.out.println("31");
+        }        
+}
+}
