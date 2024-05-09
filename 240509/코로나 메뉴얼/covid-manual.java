2e94c8866e83adca7acd552af66cbc1260a10d20
ParkDaeHee3 <156628364+ParkDaeHee3@users.noreply.github.com>
2024-05-09 21:39:33 +0900

    Title: 코로나 메뉴얼, Time: 121ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240509/코로나 메뉴얼/covid-manual.java
@@ -0,0 +1,34 @@
+import java.util.Scanner; 
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in); 
+
+        int temp1,temp2,temp3; 
+        String input1 = "Y";
+        String input2 = "N";
+
+        
+        String a =sc.next(); 
+        temp1 = sc.nextInt();
+        String b =sc.next(); 
+        temp2 = sc.nextInt();
+        String c =sc.next(); 
+        temp3 = sc.nextInt(); 
+
+        if( a.equals(input1) && temp1>=37){
+            System.out.println("E"); 
+        }
+
+        else if(b.equals(input1) && temp2>=37){
+                 System.out.println("E"); 
+                 }
+        else if (c.equals(input1) && temp3>=37){
+                  System.out.println("E"); 
+                }
+        else{
+            System.out.println("N"); 
+        }
+    }
+    
+}
