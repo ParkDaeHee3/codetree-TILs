9087d6117f65a398954be581af3ce0c244eb5e7c
ParkDaeHee3 <156628364+ParkDaeHee3@users.noreply.github.com>
2024-05-16 22:11:41 +0900

    Title: a 부터 b 까지, Time: 129ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240516/a 부터 b 까지/a-to-b.java
+++ 240516/a 부터 b 까지/a-to-b.java
@@ -5,21 +5,22 @@ public class Main {
 
         Scanner sc = new Scanner(System.in);
 
-        int a,b; 
+        int a,b,odd,even; 
 
         a = sc.nextInt();
         b = sc.nextInt();
-    
+        odd = a*2;
+        even = a+3;
         
         
-        for( int i = 1; i<=b; ){
-            if(i%2!=0 && i<b){
-                i*=2;
-                System.out.print(i+" ");
+        for( int i = 1; i<=b; i++){
+            if(i%2==0 && a<=b){
+                System.out.print(a+" ");
+                a+=3;
             }
-            else if(i%2==0 && i<b ){
-                i+=3;
-                System.out.print(i+" ");
+            else if(i%3==0 && a<=b){
+                System.out.print(a+" ");
+                a*=2;
             }
         
         }
