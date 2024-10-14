05abab022ce519b0c5852ccc9b5b24af11c710cd
ParkDaeHee3 <156628364+ParkDaeHee3@users.noreply.github.com>
2024-10-14 13:43:13 +0900

    Title: 달에서 무게 구하기, Time: 96ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 241014/달에서 무게 구하기/weight-on-the-moon.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        int a = 13; // 추의 무게 
+        double b = 0.165; // 달에서 중력의 비율 
+        double c = a * b; 
+        System.out.printf(a + " * " + "%f" + " = "+ "%f", b,c );
+        
+    }
+}
