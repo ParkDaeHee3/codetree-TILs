16c78f897854d8d2c6b5a4295d129d0c308f3c32
ParkDaeHee3 <156628364+ParkDaeHee3@users.noreply.github.com>
2025-04-09 10:24:52 +0900

    Title: 합과 평균, Time: 6ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250409/합과 평균/sum-and-avg.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+     int a,b,c;
+    
+
+     cin >> a; 
+     cin >> b; 
+
+    c = a+b; 
+
+    double d = (double)c/2;  
+    
+   
+
+    cout << c << " " << d; 
+     
+
+
+     
+
+
+    return 0;
+}
