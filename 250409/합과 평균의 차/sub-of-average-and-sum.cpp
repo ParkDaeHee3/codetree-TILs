45c30682bc4dd4428022bc86b07d3806cf08bc26
ParkDaeHee3 <156628364+ParkDaeHee3@users.noreply.github.com>
2025-04-09 10:31:02 +0900

    Title: 합과 평균의 차, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250409/합과 평균의 차/sub-of-average-and-sum.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a,b,c,d,e,f; 
+
+    cin >> a;
+    cin >> b; 
+    cin >> c; 
+
+    d = a+b+c;
+    e = d/3; 
+    f = d - e; 
+
+    cout << d; 
+    cout << "\n"; 
+    cout << e; 
+    cout << "\n"; 
+    cout << f; 
+
+    
+
+
+
+    return 0;
+}
