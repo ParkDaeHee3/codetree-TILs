7fb86151669e3b627a5c4ff2b3ef21e05118725a
ParkDaeHee3 <156628364+ParkDaeHee3@users.noreply.github.com>
2025-07-03 00:16:24 +0900

    Title: 중앙값 구하기, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

--- 250703/중앙값 구하기/find-the-median.py
+++ 250703/중앙값 구하기/find-the-median.py
@@ -1,19 +1,14 @@
 a,b,c = map(int,input().split())
 
-if (a > b) and ( a < c) :
+if ((a > b) and ( a < c)) or ((a<b) and (a>c)) or ((a>c) and (a<b))  :
     print(a)
-    if (a<b) and (a<c): 
-     print(a)
-
-if (b>a) and ( b < c) :
+    
+if ((b > a) and ( b < c)) or ((b<a) and (b>c)) or ((b>c) and (b<a))  :
     print(b)
-    if (b<a) and (b<c): 
-     print(b)
 
-if (c < a) and ( c > b) :
+if ((c > a) and ( c < b)) or ((c<a) and (c>b)) or ((c>b) and (c<a))  :
     print(c)
-    if (c>a) and (c<b): 
-     print(c)
+
      
  
  
