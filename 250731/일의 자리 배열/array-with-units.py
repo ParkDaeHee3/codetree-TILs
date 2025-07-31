63f1ccb3f96bc2e04c3f27cc890619376f505b7f
ParkDaeHee3 <156628364+ParkDaeHee3@users.noreply.github.com>
2025-07-31 13:57:21 +0900

    Title: 일의 자리 배열, Time: 43ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 250731/일의 자리 배열/array-with-units.py
@@ -0,0 +1,19 @@
+p1,p2 =tuple(list(map(int,input().split())))
+
+arr=[]
+
+arr.append(p1)
+arr.append(p2) 
+
+
+for i in range(2,10): 
+   arr.append((arr[i-2]+arr[i-1])%10) 
+
+for elem in arr: 
+    print(elem,end=" ")
+
+
+
+
+
+
